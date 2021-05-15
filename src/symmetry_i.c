90130a6f80b532a4dfbd154891325c7185df3672
Susi Lehtola <susi.lehtola@gmail.com>
2021-05-15 14:42:35 -0400

    Replace deprecated address of the Free Software Foundation with a link to their web site.

--- src/symmetry_i.c
+++ src/symmetry_i.c
@@ -16,8 +16,7 @@
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  along with this program. If not, see <https://www.gnu.org/licenses/>.
  *
  * $Log: symmetry.c,v $
  * Revision 1.16  2003/04/04  13:05:03  patchkov
