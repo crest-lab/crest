c30656cd889f992fb4c2506c8c769809c92e028e
Philipp Pracht <42216327+pprcht@users.noreply.github.com>
2022-04-28 11:52:10 +0200

    Modified CREGEN to handel relative energies in ensemble file (#113)
    Signed-off-by: Philipp Pracht <pracht@pc.rwth-aachen.de>

--- src/printouts.f90
+++ src/printouts.f90
@@ -22,7 +22,7 @@
 !===================================================================================================!
 subroutine confscript_head
       implicit none
-      character(len=40),parameter:: date='Tue 15. Feb 13:21:39 CET 2022'
+      character(len=40),parameter:: date='Thu 28. Apr 11:40:28 CEST 2022'
       character(len=10),parameter:: version='2.11.3'
       logical :: niceprint
       
